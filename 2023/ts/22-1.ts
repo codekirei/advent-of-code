eb3526171f75f11e80c16b9d9e8b67197c0e920a
Jacob Blakely <jacob@codekirei.com>
2024-01-08 18:12:48 -0800

    More progress on Day 22 part 1
    Add logic to handle tall skinny pieces (on multiple z layers).

--- 2023/ts/22-1.ts
+++ 2023/ts/22-1.ts
@@ -1,16 +1,18 @@
 // sort by lowest z
 // drop the block to min z
 // look for collisions
-// if collisions n == 1, make that block as unsafe to disint
-// return (total blocks - unsafe to disint)
 
 import { readInput } from "./utils";
 
 const input = await readInput("22");
 const blocks = input.trim().split("\n");
-const blocksCt = blocks.length;
 
-const canDisintegrate = {};
+const idToSupportedCt = {};
+const incrSupported = (id: number) => {
+  idToSupportedCt[id] = idToSupportedCt.hasOwnProperty(id)
+    ? idToSupportedCt[id] + 1
+    : 1;
+};
 
 type Coord = {
   x: number;
@@ -91,16 +93,56 @@ const sortBlocks = (a: Block, b: Block) => {
   return a.z0 - b.z0;
 };
 
+// FIXME: full grid isn't necessary; you can just keep track of height and last
+// block in one 2D array
+const grid3d = [];
 const createLayer = (x: number, y: number) => {
-  const row = Array(x).fill(null);
-  return Array(y).fill(row);
+  const createRow = () => Array(x).fill(null);
+  return Array(y).fill(null).map(createRow);
+};
+const stack = () => grid3d.push(createLayer(maxX + 1, maxY + 1));
+
+// FIXME: this is going bottom up and needs to be top down
+const dropTall = (block: Block) => {
+  let z = 0;
+  let lastSupportingId = null;
+  const { x0, y0, z0, z1, id } = block;
+
+  while (z < grid3d.length) {
+    const target = grid3d[z][y0][x0];
+    if (target == null) {
+      break;
+    }
+
+    ++z;
+    lastSupportingId = target;
+  }
+
+  if (lastSupportingId != null) {
+    incrSupported(lastSupportingId);
+  }
+
+  const z2 = z1 - z0 + z;
+  for (z; z <= z2; ++z) {
+    if (z > grid3d.length - 1) {
+      stack();
+    }
+    grid3d[z][y0][x0] = id;
+  }
 };
 
-const grid3d = [];
+const dropFlat = (block: Block) => {};
+
+const dropBlock = (block: Block) => {
+  if (block.z1 > block.z0) {
+    dropTall(block);
+  } else {
+    dropFlat(block);
+  }
+};
 
 export default function main() {
-  const orderedBlocks = blocks.map(rawToBlock).sort(sortBlocks);
-  const stack = () => grid3d.push(createLayer(maxX + 1, maxY + 1));
+  blocks.map(rawToBlock).sort(sortBlocks).forEach(dropBlock);
 }
 
 main();
